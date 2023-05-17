49e1eafb2de515d577933733ec5b1131aaf39cd9
blank.chen <blank.chen@kiwigame.com>
2023-05-17 17:28:58 +0800

    fixed path

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blankchen.github.io',
+  base: '/resume',
   integrations: [preact(), tailwind()]
 });
